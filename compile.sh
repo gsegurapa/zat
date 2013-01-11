2b700ed93ef669cb31c068e2233381dd4352d300
Chad Berkley <cberkley@flightstats.com>
2013-01-11 15:09:20 -0800

    adding deploy scripts and moving files around

---
+++ compile.sh
@@ -0,0 +1,13 @@
+#/bin/bash
+mkdir -p ${1}/build
+for js in $(find ${1}/src/*.js -maxdepth 1)
+do
+	echo "compiling ${js} in app ${1}"
+	jsstr=" --js ${js} "
+	fileparams=$fileparams$jsstr
+	count=$((count+1))
+done
+
+java -jar ./closure/compiler.jar --compilation_level SIMPLE_OPTIMIZATIONS ${fileparams} --js_output_file ${1}/build/${1}-min.js
+
+echo "${count} files compiled to  ${1}/build/${1}-min.js"

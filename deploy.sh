86e6fe1d86d8722d926763e47669384c711cd3f5
Chad Berkley <cberkley@flightstats.com>
2013-01-15 14:04:21 -0800

    created automated deployment to s3

--- deploy.sh
+++ deploy.sh
@@ -1,7 +1,15 @@
+#/bin/bash
+# usage: ./deploy <app> <bucket>
+# example: ./deploy flick flightstats-webapps-staging
 for f in $(find ./${1}/build)
 do
 	if [[ $f != *"AppleDouble"* && $f != *".git"* ]]
 	then
-		echo "deploying file ${f} in app ${1}"
+		namespace="./${1}/build"
+		length=${#namespace}+1
+		shortpath=${f:length}
+		# echo "deploying file ${f} for app ${1} to bucket ${2}/${shortpath}"
+		echo "s3put ${2}/${1}/${shortpath} ${f}"
+		s3put "${2}/${1}/${shortpath}" "${f}" 
 	fi
 done

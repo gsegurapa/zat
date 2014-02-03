99510296fb84b9a24b7aeed1a6d7d7fb3e208439
Wm Leler <wm@flightstats.com>
2014-02-02 23:35:18 -0800

    full interaction

--- globe/globe.js
+++ globe/globe.js
@@ -22,6 +22,10 @@
   var earth;  // the globe
   var airports; // airport information
   var default_airport = 'AS';
+  var highlight = null; // which flight to highlight
+  var autodata, autoindex;  // for auto parameter
+  var $mess;  // infomessage
+
 
   function getParams(st) {
     
@@ -46,23 +50,10 @@
 
   getParams(window.location.href); // params from URL
 
-  // if (appId.length === 0 || appKey.length === 0) {
-  //   if (airport.length === 0) {
-  //     appId = fleet_appId;
-  //     appKey = fleet_appKey;
-  //   } else {
-  //     appId = tracker_appId;
-  //     appKey = tracker_appKey;
-  //   }
-  // } else if (appId === fleet_appId && appKey === fleet_appKey) {
-  //   appId = '';
-  //   appKey = '';
-  //   alert('invalid appId and appKey');
-  // }
-
   $(document).ready(function() {
 
     showcode();
+    $mess = $('#automessage');
 
     var file = { regular: 'world-110m-withlakes.json', simple: 'world-50m.json' }[globe];
     earth = planetaryjs.planet();
@@ -82,23 +73,37 @@
     earth.loadPlugin(planetaryjs.plugins.zoom({
       scaleExtent: [100, 2500]
     }));
-
+    earth.loadPlugin(planetaryjs.plugins.drag({
+      onDragStart: function() { earth.plugins.autorotate.pause(); },
+      onDragEnd: function() { earth.plugins.autorotate.pause(auto !== 'rotate'); }
+    }));
+    earth.loadPlugin(autorotate(duration, auto !== 'rotate'));
     earth.loadPlugin(flights());
+    earth.loadPlugin(planetaryjs.plugins.pings());
 
-    if (auto === 'off') { // drag manually
-      earth.loadPlugin(planetaryjs.plugins.drag());
-      earth.projection.rotate([100, -20, 0]);
-      if (interactive) { $('#automessage').text('Drag globe, click on flights').show(); }
-    } else if (auto === 'rotate') {
-      earth.projection.rotate([100, -10, 0]);
-      earth.loadPlugin(autorotate(duration));
-    } else if (auto === 'flights' || auto === 'airports') {
-      earth.loadPlugin(planetaryjs.plugins.pings());
-    }
+    // initial rotation
+    earth.projection.rotate({ off: [100, -20, 0], rotate: [0, -10, 0], scroll: [100, -30, 0] }[auto]);
 
     earth.draw(document.getElementById('globe'));
 
     mainloop();
+
+    $('#zatlogo').click(function() {
+      if (auto === 'off') {
+        auto = 'rotate';
+        earth.plugins.autorotate.pause(false);
+        $mess.text('rotate').show().fadeOut(1500);
+      } else if (auto === 'rotate') {
+        auto = 'scroll';
+        earth.plugins.autorotate.pause();
+        autoindex = -1;
+        $mess.text('scroll').show();
+        autotransition();
+      } else if (auto === 'scroll') {
+        auto = 'off';
+        $mess.text('drag globe, click on airplanes').show();
+      }
+    });
 
     $('#what, #airlinelogo').click(function() {
       $('#what, #airlinelogo').hide();
@@ -163,93 +168,72 @@
     });
 
     function getFlights(data /* , status, xhr */) {
+      // console.log('flightdata', data);
       if (data.error) {
         console.log(data.error);
       }
-      // console.log('flightdata', data);
-      // $('#what').text(data.appendix.airlines[0].name);
       airports = getAppendix(data.appendix.airports);
       earth.plugins.flights.add(data.flightTracks);
+      autodata = data.flightTracks;
+      $('#globe').on('click', clickplane);
 
-      var i = -1, a, n;
-      var $mess = $('#automessage');
-
-      if (auto === 'airports') {
-        a = data.appendix.airports;
-        n = a.length;
-
-        (function transition() {
-          d3.transition().duration(duration * 1000)
-              .each('start', function() { i = (i + 1) % n; })
-              .tween('rotate', function() {
-                var p = a[i];
-                $mess.fadeOut(duration * 300, function() {
-                    $mess.text((airport.length > 0 ? (arrDep == 'dep' ? 'To ' : 'From ') : '')+airportname(p));
-                  }).delay(duration * 200).fadeIn(duration * 300, function() {
-                    earth.plugins.pings.add(p.longitude, p.latitude,
-                        { color: 'white', angle: 3, ttl: duration * 500 });
-                  });
-                var r = d3.interpolate(earth.projection.rotate(), [-p.longitude, -p.latitude]);
-                return function(t) {
-                  earth.projection.rotate(r(t));
-                };
-              })
-              .transition()
-                .each('end', transition);
-        })();
-      } else if (auto === 'flights') {
-        a = data.flightTracks;
-        n = a.length;
-
-        (function transition() {
-          d3.transition().duration(duration * 1000)
-              .each('start', function() { i = (i + 1) % n; })
-              .tween('rotate', function() {
-                var p = a[i];
-                $mess.fadeOut(duration * 300, function() {
-                    $mess.text(p.carrierFsCode+' '+p.flightNumber+': '+
-                        airportname(airports[p.departureAirportFsCode])+
-                        ' to '+airportname(airports[p.arrivalAirportFsCode]));
-                  }).delay(duration * 200).fadeIn(duration * 300, function() {
-                    earth.plugins.pings.add(p.positions[0].lon, p.positions[0].lat,
-                        { color: 'white', angle: 3, ttl: duration * 500 });
-                  });
-                var r = d3.interpolate(earth.projection.rotate(), [-p.positions[0].lon, -p.positions[0].lat]);
-                return function(t) {
-                  earth.projection.rotate(r(t));
-                };
-              })
-              .transition()
-                .each('end', transition);
-        })();
-      } else if (auto === 'off') {
-        $('#globe').on('click', clickplane);
+      if (auto === 'scroll') {
+        autoindex = -1;
         $mess.show();
+        autotransition();
+      } else if (auto === 'off') {
+        $mess.text('drag globe, click on airplanes').show();
       }
-
-
-      function clickplane(e) {  // click on plane icon to display flight info
-        var mp = earth.projection.invert([e.pageX, e.pageY]);
-        var mindist = 1000;
-        var flight = null;
-        $.each(data.flightTracks, function(i, v) {
-          var dist = d3.geo.distance(mp, [v.positions[0].lon, v.positions[0].lat]);
-          if (dist < mindist) {
-            mindist = dist;
-            flight = v;
-          }
-        });
-        if (mindist < 0.02 * scaleIcon) {
-          $mess.text(flight.carrierFsCode+' '+flight.flightNumber+': '+
-            airportname(airports[flight.departureAirportFsCode])+
-            ' to '+airportname(airports[flight.arrivalAirportFsCode])).show();
-        } else {
-          $mess.text('').hide();
-        }
-      }
-
     }
   } // end mainloop
+
+  function autotransition() { // scroll between flights
+    d3.transition().duration(duration * 1000)
+        .each('start', function() { autoindex = (autoindex + 1) % autodata.length; })
+        .tween('rotate', function() {
+          var p = autodata[autoindex],
+              pos = p.positions[0];
+          highlight = +p.flightId;
+          d3.select('#automessage').transition().duration(duration * 300).style('opacity', 0)
+            .transition().delay(duration * 400)
+                .text(p.carrierFsCode+' '+p.flightNumber+':'+
+                  (p.departureAirportFsCode !== airport ? ' from '+airportname(airports[p.departureAirportFsCode]) : '')+
+                  (p.arrivalAirportFsCode !== airport ? ' to '+airportname(airports[p.arrivalAirportFsCode]) : ''))
+            .transition().duration(duration * 300).style('opacity', 1.0)
+            .each('end', function() {
+              earth.plugins.pings.add(pos.lon, pos.lat,
+                  { color: 'white', angle: 3, ttl: duration * 500 });
+            });
+          var r = d3.interpolate(earth.projection.rotate(), [-pos.lon, -pos.lat]);
+          return function(t) {
+            earth.projection.rotate(r(t));
+          };
+        })
+        .transition()
+          .each('end', function() { if (auto === 'scroll') { autotransition(); } });
+  }
+
+  function clickplane(e) {  // click on plane icon to display flight info
+    var mp = earth.projection.invert([e.pageX, e.pageY]);
+    var mindist = 1000;
+    var flight = null;
+    $.each(autodata, function(i, v) {
+      var dist = d3.geo.distance(mp, [v.positions[0].lon, v.positions[0].lat]);
+      if (dist < mindist) {
+        mindist = dist;
+        flight = v;
+      }
+    });
+    if (mindist < 0.02 * scaleIcon) {
+      $mess.text(flight.carrierFsCode+' '+flight.flightNumber+': '+
+        airportname(airports[flight.departureAirportFsCode])+
+        ' to '+airportname(airports[flight.arrivalAirportFsCode])).show();
+      highlight = +flight.flightId;
+    } else {
+      $mess.text('').hide();
+      highlight = null;
+    }
+  }
 
   function airportname(p) { // airport information: code, city, state or country
     return p.fs+' '+p.city+' '+(p.stateCode ? p.stateCode : p.countryName);
@@ -281,6 +265,7 @@
           color: config.color,
           size: config.size,
           plane: [p.lon, p.lat],
+          id: v.flightId,
           heading: v.heading || v.bearing || 0,
           dep: [d.longitude, d.latitude],
           arr: [a.longitude, a.latitude],
@@ -293,8 +278,9 @@
       $.each(flist, function(i, v) {  // route
         context.beginPath();
         pathcontext({ type: 'LineString', coordinates: [v.dep, v.plane, v.arr] });
-        // pathcontext({ type: 'LineString', coordinates: [v.plane, v.arr] });        
-        context.strokeStyle = v.color;
+        // pathcontext({ type: 'LineString', coordinates: [v.plane, v.arr] });
+        context.strokeStyle = highlight === v.id ? '#fa2' : v.color;
+        context.lineWidth = highlight === v.id ? 3 : 1;
         context.stroke();
 
         context.beginPath();  // endpoints (airports)
@@ -304,7 +290,6 @@
       });
       $.each(flist, function(i, v) {  // airplanes
         // var plane = d3.geo.circle().origin(v.plane).angle(60 * v.size / planet.projection.scale())();
-        // console.log(v.plane[0], v.plane[1], v.heading);
         var rotfun = d3.geo.rotation([-v.plane[0], -v.plane[1], v.heading]).invert;
         var plane = { type: 'Polygon', coordinates: [icon.map(rotfun)] };
         context.beginPath();
@@ -407,10 +392,18 @@
   }
 
   // rate is in degrees per second
-  function autorotate(rate) {
+  function autorotate(rate, paused) {
+    var stop = paused === true;
     return function(planet) {
+      planet.plugins.autorotate = {
+        pause: function(b) {
+          stop = b !== false;
+          if (!stop) { tick = null; }
+        }
+      };
       var tick = null;
       planet.onDraw(function() {
+        if (stop) { return; }
         if (!tick) {
           tick = new Date();
         } else {
@@ -428,14 +421,115 @@
 
   // misc functions ----------------------------------------------
   function getAppendix(data) { // read in data from appendix and convert to dictionary
-      var ret = {};
-      if (data) {
-        for (var i = 0; i<data.length; i++) {
-          var v = data[i];
-          ret[v.fs] = v;
-        }
+    var ret = {};
+    if (data) {
+      for (var i = 0; i<data.length; i++) {
+        var v = data[i];
+        ret[v.fs] = v;
       }
-      return ret;
     }
+    return ret;
+  }
 
 }(jQuery));
+
+  // if (appId.length === 0 || appKey.length === 0) {
+  //   if (airport.length === 0) {
+  //     appId = fleet_appId;
+  //     appKey = fleet_appKey;
+  //   } else {
+  //     appId = tracker_appId;
+  //     appKey = tracker_appKey;
+  //   }
+  // } else if (appId === fleet_appId && appKey === fleet_appKey) {
+  //   appId = '';
+  //   appKey = '';
+  //   alert('invalid appId and appKey');
+  // }
+
+      // if (auto === 'airports') {
+      //   autodata = data.appendix.airports;
+      //   autolength = autodata.length;
+
+        // (function transition() {
+        //   d3.transition().duration(duration * 1000)
+        //       .each('start', function() { i = (i + 1) % autolength; })
+        //       .tween('rotate', function() {
+        //         var p = autodata[i];
+        //         $mess.fadeOut(duration * 300, function() {
+        //             $mess.text((airport.length > 0 ? (arrDep == 'dep' ? 'To ' : 'From ') : '')+airportname(p));
+        //           }).delay(duration * 200).fadeIn(duration * 300, function() {
+        //             earth.plugins.pings.add(p.longitude, p.latitude,
+        //                 { color: 'white', angle: 3, ttl: duration * 500 });
+        //           });
+        //         var r = d3.interpolate(earth.projection.rotate(), [-p.longitude, -p.latitude]);
+        //         return function(t) {
+        //           earth.projection.rotate(r(t));
+        //         };
+        //       })
+        //       .transition()
+        //         .each('end', transition);
+        // })();
+      // } else
+
+        // (function transition() {
+        //   d3.transition().duration(duration * 1000)
+        //       .each('start', function() {i = (i + 1) % autolength; })
+        //       .tween('rotate', function() {
+        //         var p = autodata[i];
+        //         $mess.fadeOut(duration * 300, function() {
+        //             $mess.text(p.carrierFsCode+' '+p.flightNumber+': '+
+        //                 airportname(airports[p.departureAirportFsCode])+
+        //                 ' to '+airportname(airports[p.arrivalAirportFsCode]));
+        //           }).delay(duration * 200).fadeIn(duration * 300, function() {
+        //             earth.plugins.pings.add(p.positions[0].lon, p.positions[0].lat,
+        //                 { color: 'white', angle: 3, ttl: duration * 500 });
+        //           });
+        //         var r = d3.interpolate(earth.projection.rotate(), [-p.positions[0].lon, -p.positions[0].lat]);
+        //         return function(t) {
+        //           earth.projection.rotate(r(t));
+        //         };
+        //       })
+        //       .transition()
+        //         .each('end', transition);
+        // })();
+
+  // helper function to find control point for quadratic curve to draw curve through 3 points
+//   function findControlPoint(s1, s2, s3) {
+//     var // Unit vector, length of line s1,s3
+//         ux1 = s3.x - s1.x,
+//         uy1 = s3.y - s1.y,
+//         ul1 = Math.sqrt(ux1*ux1 + uy1*uy1)
+//         u1 = { x: ux1/ul1, y: uy1/ul1 },
+ 
+//         // Unit vector, length of line s1,s2
+//         ux2 = s2.x - s1.x,
+//         uy2 = s2.y - s1.y,
+//         ul2 = Math.sqrt(ux2*ux2 + uy2*uy2),
+//         u2 = { x: ux2/ul2, y: uy2/ul2 },
+ 
+//         // Dot product
+//         k = u1.x*u2.x + u1.y*u2.y,
+ 
+//         // Project s2 onto s1,s3
+//         il1 = { x: s1.x+u1.x*k*ul2, y: s1.y+u1.y*k*ul2 },
+ 
+//         // Unit vector, length of s2,il1
+//         dx1 = s2.x - il1.x,
+//         dy1 = s2.y - il1.y,
+//         dl1 = Math.sqrt(dx1*dx1 + dy1*dy1),
+//         d1 = { x: dx1/dl1, y: dy1/dl1 },
+ 
+//         // Midpoint
+//         mp = { x: (s1.x+s3.x)/2, y: (s1.y+s3.y)/2 },
+ 
+//         // Control point on s2,il1
+//         cpm = { x: s2.x+d1.x*dl1, y: s2.y+d1.y*dl1 },
+ 
+//         // Translate based on distance from midpoint
+//         tx = il1.x - mp.x,
+//         ty = il1.y - mp.y,
+//         cp = { x: cpm.x+tx, y: cpm.y+ty };
+ 
+//     return cp;
+// }
